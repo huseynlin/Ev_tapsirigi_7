874c5a64b81eb966319f8e9780836c6749a738e4
huseynlin <nuralihuseynli53@gmail.com>
2025-10-15 07:06:58 +0400

    classroom

---
+++ Ev_tapsirigi_7/Classroom_rezerv/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace Classroom_rezerv
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            maskedTextBox1 = new MaskedTextBox();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            button2 = new Button();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            maskedTextBox2 = new MaskedTextBox();
+            label5 = new Label();
+            label6 = new Label();
+            groupBox3 = new GroupBox();
+            button3 = new Button();
+            label7 = new Label();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            maskedTextBox3 = new MaskedTextBox();
+            label8 = new Label();
+            label9 = new Label();
+            groupBox4 = new GroupBox();
+            button4 = new Button();
+            label10 = new Label();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            maskedTextBox4 = new MaskedTextBox();
+            label11 = new Label();
+            label12 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(groupBox1, "groupBox1");
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(maskedTextBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.ForeColor = Color.White;
+            groupBox1.Name = "groupBox1";
+            groupBox1.TabStop = false;
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(button1, "button1");
+            button1.BackColor = Color.Green;
+            button1.Cursor = Cursors.Hand;
+            button1.Name = "button1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.Name = "label3";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(textBox2, "textBox2");
+            textBox2.Name = "textBox2";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(textBox1, "textBox1");
+            textBox1.Name = "textBox1";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // maskedTextBox1
+            // 
+            resources.ApplyResources(maskedTextBox1, "maskedTextBox1");
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.ValidatingType = typeof(int);
+            maskedTextBox1.MaskInputRejected += maskedTextBox1_MaskInputRejected;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            // 
+            // groupBox2
+            // 
+            resources.ApplyResources(groupBox2, "groupBox2");
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(textBox3);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(maskedTextBox2);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label6);
+            groupBox2.ForeColor = Color.White;
+            groupBox2.Name = "groupBox2";
+            groupBox2.TabStop = false;
+            groupBox2.Enter += groupBox2_Enter;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(button2, "button2");
+            button2.BackColor = Color.Green;
+            button2.Cursor = Cursors.Hand;
+            button2.Name = "button2";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(label4, "label4");
+            label4.Name = "label4";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(textBox3, "textBox3");
+            textBox3.Name = "textBox3";
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(textBox4, "textBox4");
+            textBox4.Name = "textBox4";
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // maskedTextBox2
+            // 
+            resources.ApplyResources(maskedTextBox2, "maskedTextBox2");
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.ValidatingType = typeof(int);
+            maskedTextBox2.MaskInputRejected += maskedTextBox2_MaskInputRejected;
+            // 
+            // label5
+            // 
+            resources.ApplyResources(label5, "label5");
+            label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(label6, "label6");
+            label6.Name = "label6";
+            // 
+            // groupBox3
+            // 
+            resources.ApplyResources(groupBox3, "groupBox3");
+            groupBox3.Controls.Add(button3);
+            groupBox3.Controls.Add(label7);
+            groupBox3.Controls.Add(textBox5);
+            groupBox3.Controls.Add(textBox6);
+            groupBox3.Controls.Add(maskedTextBox3);
+            groupBox3.Controls.Add(label8);
+            groupBox3.Controls.Add(label9);
+            groupBox3.ForeColor = Color.White;
+            groupBox3.Name = "groupBox3";
+            groupBox3.TabStop = false;
+            groupBox3.Enter += groupBox3_Enter;
+            // 
+            // button3
+            // 
+            resources.ApplyResources(button3, "button3");
+            button3.BackColor = Color.Green;
+            button3.Cursor = Cursors.Hand;
+            button3.Name = "button3";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // label7
+            // 
+            resources.ApplyResources(label7, "label7");
+            label7.Name = "label7";
+            // 
+            // textBox5
+            // 
+            resources.ApplyResources(textBox5, "textBox5");
+            textBox5.Name = "textBox5";
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // textBox6
+            // 
+            resources.ApplyResources(textBox6, "textBox6");
+            textBox6.Name = "textBox6";
+            textBox6.TextChanged += textBox6_TextChanged;
+            // 
+            // maskedTextBox3
+            // 
+            resources.ApplyResources(maskedTextBox3, "maskedTextBox3");
+            maskedTextBox3.Name = "maskedTextBox3";
+            maskedTextBox3.ValidatingType = typeof(int);
+            maskedTextBox3.MaskInputRejected += maskedTextBox3_MaskInputRejected;
+            // 
+            // label8
+            // 
+            resources.ApplyResources(label8, "label8");
+            label8.Name = "label8";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(label9, "label9");
+            label9.Name = "label9";
+            // 
+            // groupBox4
+            // 
+            resources.ApplyResources(groupBox4, "groupBox4");
+            groupBox4.Controls.Add(button4);
+            groupBox4.Controls.Add(label10);
+            groupBox4.Controls.Add(textBox7);
+            groupBox4.Controls.Add(textBox8);
+            groupBox4.Controls.Add(maskedTextBox4);
+            groupBox4.Controls.Add(label11);
+            groupBox4.Controls.Add(label12);
+            groupBox4.ForeColor = Color.White;
+            groupBox4.Name = "groupBox4";
+            groupBox4.TabStop = false;
+            groupBox4.Enter += groupBox4_Enter;
+            // 
+            // button4
+            // 
+            resources.ApplyResources(button4, "button4");
+            button4.BackColor = Color.Green;
+            button4.Cursor = Cursors.Hand;
+            button4.Name = "button4";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // label10
+            // 
+            resources.ApplyResources(label10, "label10");
+            label10.Name = "label10";
+            // 
+            // textBox7
+            // 
+            resources.ApplyResources(textBox7, "textBox7");
+            textBox7.Name = "textBox7";
+            textBox7.TextChanged += textBox7_TextChanged;
+            // 
+            // textBox8
+            // 
+            resources.ApplyResources(textBox8, "textBox8");
+            textBox8.Name = "textBox8";
+            textBox8.TextChanged += textBox8_TextChanged;
+            // 
+            // maskedTextBox4
+            // 
+            resources.ApplyResources(maskedTextBox4, "maskedTextBox4");
+            maskedTextBox4.Name = "maskedTextBox4";
+            maskedTextBox4.ValidatingType = typeof(int);
+            maskedTextBox4.MaskInputRejected += maskedTextBox4_MaskInputRejected;
+            // 
+            // label11
+            // 
+            resources.ApplyResources(label11, "label11");
+            label11.Name = "label11";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(label12, "label12");
+            label12.Name = "label12";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Teal;
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private MaskedTextBox maskedTextBox1;
+        private Label label2;
+        private Label label1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private GroupBox groupBox4;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private MaskedTextBox maskedTextBox2;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private MaskedTextBox maskedTextBox3;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private MaskedTextBox maskedTextBox4;
+        private Label label11;
+        private Label label12;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
